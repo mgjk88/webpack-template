e418439700ffe39c0bd3643c13a7041e29c01e11
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2024-12-27 20:04:14 +0800

    split webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -5,19 +5,15 @@ module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
-    ],
+    ], 
     module: {
         rules: [
             {
@@ -33,5 +29,5 @@ module.exports = {
                 type: "asset/resource",
             },
         ],
-    }, 
+    }
 };
