1861a7548e642b1f10754a86039676b1d47e3d53
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2025-01-04 15:59:40 +0800

    adjust eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ["webpack/*"] },
   pluginJs.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ];
-
